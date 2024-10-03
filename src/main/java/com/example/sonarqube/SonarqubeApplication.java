38a0a95a402494806957bfd749b26d77e9a3fa52
Prempavan Gunda <prempavan_gunda@thbs.india.com>
2024-10-03 16:49:15 +0530

    hello

---
+++ src/main/java/com/example/sonarqube/SonarqubeApplication.java
@@ -0,0 +1,14 @@
+package com.example.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Prem....");
+		SpringApplication.run(SonarqubeApplication.class, args);
+	}
+
+}
